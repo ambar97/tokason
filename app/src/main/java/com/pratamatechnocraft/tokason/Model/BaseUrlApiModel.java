cfe4b026d96cee059c398348694cbdf70d643754
ambar97 <abiemassyikin@gmail.com>
2020-01-18 11:12:46 +0700

    new server

--- app/src/main/java/com/pratamatechnocraft/tokason/Model/BaseUrlApiModel.java
+++ app/src/main/java/com/pratamatechnocraft/tokason/Model/BaseUrlApiModel.java
@@ -2,7 +2,7 @@ package com.pratamatechnocraft.tokason.Model;
 
 public class BaseUrlApiModel {
     //private String baseURL="http://192.168.1.7/proyek/Si_Laporan_Penjualan/index.php/";
-    private String baseURL="https://tokason.pratamatechnocraft.com/";
+    private String baseURL="http://64310715e142.sn.mynetname.net/tokason_api/";
 
     public String getBaseURL() {
         return baseURL;
