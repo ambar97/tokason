eba0a1fd1e835199a444cae380d9e38636b9839e
ambar97 <abiemassyikin@gmail.com>
2020-02-01 15:30:40 +0700

    hhhhh

--- app/src/main/java/com/pratamatechnocraft/tokason/Fragment/WaitingConfirmationFragment.java
+++ app/src/main/java/com/pratamatechnocraft/tokason/Fragment/WaitingConfirmationFragment.java
@@ -1,21 +1,39 @@
 package com.pratamatechnocraft.tokason.Fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.pratamatechnocraft.tokason.MainActivity;
+import com.pratamatechnocraft.tokason.Model.BaseUrlApiModel;
 import com.pratamatechnocraft.tokason.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class WaitingConfirmationFragment extends Fragment {
 
     Context context;
+    BaseUrlApiModel baseUrlApiModel = new BaseUrlApiModel();
+    private String baseUrl=baseUrlApiModel.getBaseURL();
+    private static final String API_URL_LOAD = "api/user?api=dataprofile&kd_user=";
+    private static final String API_URL = "api/user";
 
     public WaitingConfirmationFragment() {
         // Required empty public constructor
@@ -39,9 +57,40 @@ public class WaitingConfirmationFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 // TODO: 1/29/2020 Cek Status User, jika sudah dibayar masuk ke Main Activity
+
             }
         });
 
+
+    }
+    private void loadProfile(final String kd_user){
+        StringRequest stringRequest = new StringRequest( Request.Method.GET, baseUrl+API_URL_LOAD+kd_user,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            Log.d("TAG", "onResponse: "+response);
+                            final JSONObject userprofile = new JSONObject(response);
+                            if (userprofile.getString("status").equals("1")){
+                                startActivity(new Intent(context, MainActivity.class));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(context, "Periksa koneksi & coba lagi1", Toast.LENGTH_SHORT).show();
+                            Log.e("TAG", "onResponse: ", e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, "Periksa koneksi & coba lagi", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        RequestQueue requestQueue = Volley.newRequestQueue( context );
+        requestQueue.add( stringRequest );
 
     }
 }
